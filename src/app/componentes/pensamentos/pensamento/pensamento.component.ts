0c8d54592800008b5d44578eb61311efa3e5ab64
Bruno <brunocosta.1991@hotmail.com>
2022-09-19 19:24:53 -0300

    diretivas

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.scss'],
+})
+export class PensamentoComponent implements OnInit {
+  @Input() pensamento: any;
+  constructor() {}
+
+  ngOnInit(): void {}
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) return 'pensamento-g';
+
+    return 'pensamento-p';
+  }
+}
